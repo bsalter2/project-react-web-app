c8466746366c5a2aacce9218e88ab31ede8cada0
bsalter2 <brendansalter97@gmail.com>
2023-06-08 15:00:43 -0700

    added bootstrap, routes, and pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter, Navigate} from "react-router-dom"
+import {Routes, Route} from "react-router"
+import Search from './search-page';
+import Home from './home-page';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   return (
+      <BrowserRouter>
+        <div className="container">
+        <Routes>
+          <Route path="/" element={<Navigate to="/home"/>}/>
+          <Route path="/home/*" element={<Home/>}/>
+          <Route path="/search/*" element={<Search/>}/>
+        </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
