ef5c0d9d7152323476338ec12b3f104172b1cd59
David <daviddadiomov@gmail.com>
2023-06-24 16:38:27 -0700

    admin changes

--- src/admin-page/admin.js
+++ src/admin-page/admin.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllUsersThunk } from "../services/users-thunk";
+import { getAllUsersThunk, deleteUserThunk } from "../services/users-thunk";
 import styled from 'styled-components';
 
 const UserItem = styled.div`
@@ -24,6 +24,20 @@ const UserDetails = styled.p`
   margin-bottom: 0.5rem;
 `;
 
+const DeleteButton = styled.button`
+  padding: 5px 10px;
+  margin-left: 10px;
+  color: #fff;
+  background-color: #ff6347; 
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #ff0000; 
+  }
+`;
+
 function Admin() {
   const dispatch = useDispatch();
   const { users } = useSelector((state) => state.users);
@@ -31,6 +45,10 @@ function Admin() {
   useEffect(() => {
     dispatch(getAllUsersThunk());
   }, [dispatch]);
+
+  const handleDelete = (id) => {
+    dispatch(deleteUserThunk(id));
+  };
 
   return (
     <div className="container">
@@ -41,6 +59,7 @@ function Admin() {
             <UserDetails>First Name: {user.firstName}</UserDetails>
             <UserDetails>Last Name: {user.lastName}</UserDetails>
             {/* <UserDetails>Email: {user.email}</UserDetails> */}
+            <DeleteButton onClick={() => handleDelete(user.id)}>Delete</DeleteButton>
           </UserItem>
         ))}
       </div>
@@ -49,4 +68,3 @@ function Admin() {
 }
 
 export default Admin;
-
